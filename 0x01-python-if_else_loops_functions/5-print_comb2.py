bdc7d8109823362b4f8767c5896957c5b6c72b89
destinyOrji <destinyorji18@gmail.com>
2023-11-01 17:16:13 -0700

    a program that prints numbers from 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for x == 99:
-    print("{}".format(x))
+for x in range(0, 100):
+    if x == 99:
+        print("{}".format(x))
     else:
         print("{:02}".format(x), end=", ")

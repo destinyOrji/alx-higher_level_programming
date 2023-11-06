91ac093bc5fa8ea65578f75f4fbebbc0012474bf
destinyOrji <destinyorji18@gmail.com>
2023-11-06 14:41:57 -0800

    update

---
+++ 0x03-python-data_structures/100-test_lists.py
@@ -0,0 +1,7 @@
+#include <python>
+
+/**
+ * print_python_list _info - printd basic info about python lists.
+ * @p: a pyobjectlist
+ */
+void 
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,29 +1,24 @@
-#ifndef LISTS.H
-#define LISTS.H
-
-#include <stdlib.h>
-#include <stdio.h>
-#include < imistd.h>
+#ifndef LISTS_H
+#define LISTS_H
 
 /**
- * strct listint_s - singly linked list
- * @n: interger
+ * struct listint_s - singly linked list
+ * @n: integer
  * @next: points to the next node
  *
- * description: singly linked list node structure
- * for holbertion project
+ * Description: singly linked list node structure
+ * for project
  */
 typedef struct listint_s
 {
-	int n:
-	struct listint_s *next:
-} listint_t
+    int n;
+    struct listint_s *next;
+} listint_t;
 
-size_t print_listint(const listint_t *h):
-listint_t *add_nodeint_end(listint_t **head, const int n):
-void free_listint(listint_t *head):
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
 
-void reverse_listint(listint_t **head):
-int is_palindrome(listint_t **head):
+int is_palindrome(listint_t **head);
 
-#endif/* LIST_H */
+#endif /* LISTS_H */

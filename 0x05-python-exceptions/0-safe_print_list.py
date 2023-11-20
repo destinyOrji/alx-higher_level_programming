e6281f1b740fac9f12d11381648ab9721ba04ec9
destinyOrji <destinyorji18@gmail.com>
2023-11-20 13:31:13 -0800

    a function that prints x elements of a list,All elements must be printed on the same line followed by a new line

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
     printed_count = 0
-
     try:
         iterator = iter(my_list)
         while printed_count < x:
             print(next(iterator), end=' ')
             printed_count += 1
     except StopIteration:
-        pass  
-
-    print()  
+        pass
+    print()
     return printed_count

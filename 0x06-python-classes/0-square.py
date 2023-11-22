9398798a083c2d3a9842228a5a304646c89a1a6d
destinyOrji <destinyorji18@gmail.com>
2023-11-21 20:57:29 -0800

    an empty class Square that defines a square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-#0-square.py br orji destiny
-"""creating an empty class Square"""
+# 0-square.py by orji destiny
+"""creating an emptySquare class"""
 
 class Square:
     """type: an empty class square that define a square"""

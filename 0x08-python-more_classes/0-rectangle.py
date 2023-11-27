b554e2e5b8d2f15e9c646a0094cc44bf32406154
destinyOrji <destinyorji18@gmail.com>
2023-11-27 14:37:16 -0800

    Write an empty class Rectangle that defines a rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+
+# Define an empty class Rectangle
 class Rectangle:
+    # The 'pass' statement is a placeholder that does nothing.
+    # It is used here to ensure that the class is not empty,
+    # as Python requires at least one statement in a code block.
     pass
 
